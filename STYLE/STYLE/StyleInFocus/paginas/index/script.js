6acb6958cb1838c466ba76bd36223d7e337a8946
ViniciusReis28 <vininaetec@gmail.com>
2024-09-19 16:16:53 -0300

    alguns problemas

--- STYLE/STYLE/StyleInFocus/paginas/index/script.js
+++ STYLE/STYLE/StyleInFocus/paginas/index/script.js
@@ -244,3 +244,23 @@ document.addEventListener('DOMContentLoaded', function() {
       });
   });
 });
+
+
+var radio = document.querySelector('.manual-btn')
+var cont = 1
+
+document.getElementById('radio1').checked = true
+
+setInterval(() => {
+    proximaimg()
+}, 4000)
+
+function proximaimg(){
+    cont++
+
+    if(cont > 3 ){
+        cont = 1
+    }
+
+    document.getElementById('radio'+cont).checked = true
+}
