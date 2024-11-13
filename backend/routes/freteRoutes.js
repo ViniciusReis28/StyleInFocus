8522b4874e00d7b175e5434b40eeca3672b67d96
Gabriel Pablo <pablobocudo.lima@gmail.com>
2024-11-12 23:30:49 -0300

    teste

--- backend/routes/freteRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const freteController = require('../controllers/freteController'); // Importando o controller de frete
-const router = express.Router();
-
-// Rota para calcular o frete
-router.post('/calcular-frete', freteController.calcularFrete);
-
-module.exports = router;
