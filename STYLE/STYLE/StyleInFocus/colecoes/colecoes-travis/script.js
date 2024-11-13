3f7776073a33eb7f3ccb32fdaab254d06a27633c
ViniciusReis28 <vininaetec@gmail.com>
2024-11-12 22:51:43 -0300

    aaa

--- STYLE/STYLE/StyleInFocus/colecoes/colecoes-travis/script.js
+++ STYLE/STYLE/StyleInFocus/colecoes/colecoes-travis/script.js
@@ -1,36 +1,3 @@
-document.getElementById("profile-icon").onclick = function () {
-  document.getElementById("sidebar").style.width = "350px";
-  document.getElementById("overlay").style.width = "100%";
-};
-
-document.querySelector(".close-btn").onclick = function () {
-  document.getElementById("sidebar").style.width = "0";
-  document.getElementById("overlay").style.width = "0";
-};
-
-document.getElementById("overlay").onclick = function () {
-  document.getElementById("sidebar").style.width = "0";
-  document.getElementById("overlay").style.width = "0";
-};
-
-
-function openLeftSidebar() {
-  document.getElementById("leftSidebar").style.width = "380px";
-}
-
-function closeLeftSidebar() {
-  document.getElementById("leftSidebar").style.width = "0";
-}
-
-// Fechar o sidebar ao clicar fora dele
-document.addEventListener('click', function(event) {
-  var sidebar = document.getElementById('leftSidebar');
-  var btn = document.querySelector('.btn-filtrar');
-  if (sidebar.style.width === "380px" && !sidebar.contains(event.target) && event.target !== btn) {
-      closeLeftSidebar();
-  }
-});
-
 function toggleDropdown(dropdownId) {
   const dropdown = document.getElementById(dropdownId);
   const arrow = dropdown.previousElementSibling.querySelector('.arrow');
@@ -52,7 +19,6 @@ function toggleColor(color) {
       button.classList.add('selected');
   }
 }
-
 window.addEventListener('scroll', function() {
   const nav = document.querySelector('nav');
   if (window.scrollY > 50) { // Ajuste o valor conforme necessário
@@ -78,10 +44,10 @@ window.addEventListener('scroll', function() {
 
   if (window.scrollY > 50) { // Ajuste o valor conforme necessário
     nav.classList.add('scrolled'); // Adiciona a classe quando rola para baixo
-    logo.src = '../../img/logopreta.png'; // Altera para a imagem preta
+    logo.src = '../../../img/logopreta.png'; // Altera para a imagem preta
   } else {
     nav.classList.remove('scrolled'); // Remove a classe quando rola para cima
-    logo.src = '../../img/logobranca.png'; // Retorna à imagem branca
+    logo.src = '../../../img/logobranca.png'; // Retorna à imagem branca
   }
 });
 
@@ -147,8 +113,6 @@ setInterval(nextSlide, slideInterval);
 document.querySelector('.next').addEventListener('click', nextSlide);
 document.querySelector('.prev').addEventListener('click', prevSlide);
 });
-
-
 
 
 
