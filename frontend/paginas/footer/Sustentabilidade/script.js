8149da5d0d7aa24d528b655dbb55253cf04babe3
ViniciusReis28 <vininaetec@gmail.com>
2024-12-03 17:58:32 -0300

    Update script.js

--- frontend/paginas/footer/Sustentabilidade/script.js
+++ frontend/paginas/footer/Sustentabilidade/script.js
@@ -113,25 +113,6 @@ function toggleDropdown(dropdownId) {
   });
   
   
-  var radio = document.querySelector('.manual-btn')
-  var cont = 1
-  
-  document.getElementById('radio1').checked = true
-  
-  setInterval(() => {
-      proximaimg()
-  }, 4000)
-  
-  function proximaimg(){
-      cont++
-  
-      if(cont > 3 ){
-          cont = 1
-      }
-  
-      document.getElementById('radio'+cont).checked = true
-  }
-  
   
   // Simulação de produtos disponíveis com URLs
   const products = [
